1fa0f83a193bab779763c18f6f7464c15113afc2
Antonio Eugenio Herreros Caballero <antonioherreroscaballero@gmail.com>
2022-05-27 19:19:32 +0200

    Inicio
    Creación app webmap

---
+++ JS/capitalesromanas.js
@@ -0,0 +1,13 @@
+var capitales = {
+      "type": "FeatureCollection",
+  "features": [
+    { "type": "Feature", "properties": {"Provincia":"Lusitania","capital":"Augusta Emerita","foto":"Augusta_Emerita.jpg","link":"https://es.wikipedia.org/wiki/Augusta_Emerita"},
+      "geometry": {"type": "Point","coordinates": [-6.3370, 38.9146]}
+    },
+    { "type": "Feature", "properties": {"Provincia":"Baetica","capital": "Corduba", "foto":"Corduba.jpg", "link":"https://es.wikipedia.org/wiki/Corduba" },
+     "geometry": {"type": "Point","coordinates": [-4.76531,37.8835]}
+    },
+    {"type": "Feature", "properties": {"Provincia":"Tarraconiense", "capital": "Tarraco","foto":"Tarraco.jpg","link":"https://es.wikipedia.org/wiki/Tarraco"},
+ "geometry": {"type": "Point", "coordinates":[1.2589, 41.1146]}}
+  ]
+};
